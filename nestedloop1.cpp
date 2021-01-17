b0145f996f13538647bec4f52af136adf11cfb22
Nikhil Bansod <73024614+NikhiIbansod@users.noreply.github.com>
2021-01-17 16:08:48 +0530

    Add files via upload

---
+++ nestedloop1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{ 
+         for(int a=9;a>=6;a--)
+  {
+    for(int i=1;i<=a;i++) 
+       cout<<i;    
+    cout<<endl;
+  }
+ return 0; 
+}
