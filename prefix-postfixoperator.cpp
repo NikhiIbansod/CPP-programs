ad3ec3ffac5d8018fcff199e03222777c7685136
Nikhil Bansod <73024614+NikhiIbansod@users.noreply.github.com>
2021-01-19 13:11:34 +0530

    Add files via upload

---
+++ prefix-postfixoperator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=3,b=3;
+     
+    cout<<a++;
+    cout<<++a;
+           cout<<endl;
+    cout<<++b;
+    cout<<b++;
+
+    return 0;
+}
